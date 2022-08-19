b502b2df2114729ba732feb07803d3231b1163dd
邱旭 <uxuip@outlook.com>
2022-08-19 16:02:41 +0800

    build: 修改vant安装目录

--- scripts/install-vant.js
+++ scripts/install-vant.js
@@ -1,9 +1,24 @@
-const fs = require('fs')
+const fs = require('fs/promises')
+const path = require('path')
 
-// 复制vant组件到wxcomponents目录
-fs.cp('./node_modules/@vant', './src/wxcomponents/@vant', {
-  recursive: true,
-  dereference: true,
-}, (err) => {
-  if (err) console.error(err)
-})
+const vantDir = path.join(process.env.INIT_CWD, './node_modules/@vant/weapp/dist')
+const targetDir = path.join(process.env.INIT_CWD, './src/wxcomponents/vant')
+
+async function setup() {
+  // 清空目录
+  await fs.rm(targetDir, {
+    recursive: true,
+    force: true,
+  })
+
+  // 复制vant组件到wxcomponents目录
+  await fs.cp(vantDir, targetDir, {
+    recursive: true,
+    dereference: true,
+  })
+
+  // 忽略目录
+  await fs.writeFile(path.join(targetDir, '.gitignore'), '*')
+}
+
+setup().catch(console.error)
